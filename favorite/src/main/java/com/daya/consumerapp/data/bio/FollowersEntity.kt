507ca2fd8b1390fdd71934c262817cea8090bb76
ad <da>
2021-07-18 18:32:59 +0700

    add favorite module and remove consumer

--- favorite/src/main/java/com/daya/consumerapp/data/bio/FollowersEntity.kt
+++
@@ -1,8 +0,0 @@
-package com.daya.consumerapp.data.bio
-data class FollowersEntity(
-    val followersUserName: String,
-
-    val userName :String,
-
-    val avatarUrl: String
-)
--- favorite/src/main/java/com/daya/consumerapp/data/bio/FollowingEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.daya.consumerapp.data.bio
-data class FollowingEntity(
-
-    val followingUserName: String,
-
-    val userName :String,
-
-    val avatarUrl: String
-)
